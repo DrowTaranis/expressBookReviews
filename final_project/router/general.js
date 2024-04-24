44be2b0e6695dd7e637cd51e7a146abe3873a9c8
DrowTaranis <olieclay@hotmail.com>
2024-04-24 11:25:59 -0400

    Added GET book by title and GET reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,14 +36,21 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const titleA = req.params.title;
+    const filteredTitles = [];
+    const keys = Object.keys(books);
+    keys.forEach(key => {
+        if (books[key].title === titleA) {
+            filteredTitles.push(books[key]);
+        }
+    });
+    res.send(filteredTitles);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]["reviews"])
 });
 
 module.exports.general = public_users;
