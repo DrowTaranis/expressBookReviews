518da2d340980bc81e2df320490526bf52df1787
DrowTaranis <olieclay@hotmail.com>
2024-04-24 11:21:30 -0400

    Added GET by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const authorA = req.params.author;
+    const filteredAuthors = [];
+    const keys = Object.keys(books);
+    keys.forEach(key => {
+        if (books[key].author === authorA) {
+            filteredAuthors.push(books[key]);
+        }
+    });
+    res.send(filteredAuthors);
 });
 
 // Get all books based on title
